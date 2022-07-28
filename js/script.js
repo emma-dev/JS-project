aee87ba86f32cf463119e7a4b0b45c4c0989d7ac
Sveta <example@gmai.com>
2022-07-28 12:59:45 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+
+"use sctrict";
+
+
+console.log('arr' + "- object");
+
+
+let incr = 10,
+    decr = 10;
+
+
+console.log(++incr);
+console.log(--decr);
+
+
+console.log(5%2);
+
+console.log(2*4 === 8);
+
+
+const isChecked= true, isClose= true;
+console.log(isChecked && isClose); 
